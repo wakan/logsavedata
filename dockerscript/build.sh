4d8970e0436bd4f496ef3f3ed04d949f7640c97e
root <root@n1.lan>
2019-09-05 15:38:06 +0200

    docker recorder

--- dockerscript/build.sh
+++ dockerscript/build.sh
@@ -2,4 +2,5 @@
 
 docker build --no-cache -t domminatrix/logsavedataconfigcaller:latest src/test/config/configcaller
 docker build --no-cache -t domminatrix/logsavedataconfiglistener:latest src/main/config/configlistener
-docker build --no-cache -t domminatrix/forecastconfigcalltester:latest src/test/config/configcalltester
+docker build --no-cache -t domminatrix/logsavedataconfigcalltester:latest src/test/config/configcalltester
+docker build --no-cache -t domminatrix/logsavedatarunrecorder:latest src/main/run/recorder
